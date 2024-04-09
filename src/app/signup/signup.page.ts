b5377922975edda5560584dcffbd2102027d5fdc
SamyaAbouKhamis <rouaba134@gmail.com>
2024-04-09 17:28:20 +0300

    update login form

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,9 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 
 @Component({
@@ -11,12 +12,34 @@ import { RouterLink } from '@angular/router';
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule,RouterLink,ReactiveFormsModule, HttpClientModule,
+    HttpClientJsonpModule]
  
   
 })
 export class SignupPage implements OnInit {
-
+countries:any=[
+  {
+  code:'+963',
+  name:'syria'
+  },
+  {
+    code:'963+',
+    name:'syria',
+  },
+  {
+    code:'963+',
+    name:'syria',
+  },
+  {
+    code:'963+',
+    name:'syria',
+  },
+  {
+    code:'963+',
+    name:'syria',
+  }
+]
   constructor() { }
 
   ngOnInit() {
